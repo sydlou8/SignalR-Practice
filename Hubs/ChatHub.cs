608fef83c2f13385cd4066f0e84532245230e019
Sydney Morano <syd.morano@gmail.com>
2024-10-25 10:10:51 -0700

    added to builder

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,20 +1,19 @@
 using Fxst.ChatService.Models;
 using Microsoft.AspNetCore.SignalR;
 
-namespace Fxst.ChatService.Hubs
-{
-    public class ChatHub : Hub {
-        public async Task JoinChat(UserConnection userConnection) {
-            // Hub gives us access to the Clients property, which allows us to send messages to all connected clients
-            await Clients.All // Send to all connected clients
-                .SendAsync("ReceiveMessage", "System", $"{userConnection.UserName} has joined the chat.");
-        }
+namespace Fxst.ChatService.Hubs;
+
+public class ChatHub : Hub {
+    public async Task JoinChat(UserConnection userConnection) {
+        // Hub gives us access to the Clients property, which allows us to send messages to all connected clients
+        await Clients.All // Send to all connected clients
+            .SendAsync("ReceiveMessage", "System", $"{userConnection.UserName} has joined the chat.");
+    }
         
-        public async Task JoinSpecificChatRoom(UserConnection userConnection) {
-            // Hub gives us access to the Groups property, which allows us to send messages to all clients in a specific group
-            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.ChatRoom);
-            await Clients.Group(userConnection.ChatRoom) // Send to all clients in the specified group
-                .SendAsync("ReceiveMessage", "System", $"{userConnection.UserName} has joined {userConnection.ChatRoom}.");
-        }
+    public async Task JoinSpecificChatRoom(UserConnection userConnection) {
+        // Hub gives us access to the Groups property, which allows us to send messages to all clients in a specific group
+        await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.ChatRoom);
+        await Clients.Group(userConnection.ChatRoom) // Send to all clients in the specified group
+            .SendAsync("ReceiveMessage", "System", $"{userConnection.UserName} has joined {userConnection.ChatRoom}.");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSignalR();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,5 +22,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<ChatHub>("/Chat");
 
 app.Run();
